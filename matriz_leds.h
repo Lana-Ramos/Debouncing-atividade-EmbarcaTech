03454639cd7cd90d5f31b88b900ab2cad8e77ffb
Lana Ramos <lanadefy@gmail.com>
2025-02-01 21:54:50 -0300

    preenchendo os protótipos das funções

--- matriz_leds.h
+++ matriz_leds.h
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "pico\stdlib.h"
 
-#define MATRIZ_LED 7; // Pino que a matriz de leds está conectada
+#define MATRIZ_LEDS 7; // Pino que a matriz de leds está conectada
 #define N_LEDS 25; // número de leds na matrix 5x5
 
-void inicializar_leds();
+// Estrutura que armazena as cores RGB
+typedef struct 
+{
+    uint8_t G,R,B;
+}npLED_t;
 
+npLED_t leds[N_LEDS];
+void inicializar_leds();
 void blink_red();
 
