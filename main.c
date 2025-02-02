eea3ec39045748c0544e51903f6e6463cb60776d
Lana Ramos <lanadefy@gmail.com>
2025-02-01 21:55:40 -0300

    Adicionando o arquivo com as funções do led à main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "pico\stdlib.h"
 
+#include "matriz_leds.h"
 #define MATRIZ_LED 7; // Pino que a matriz de leds está conectada
 #define N_LEDS 25; // número de leds na matrix 5x5
